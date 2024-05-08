c7455d712bb4e4663e81b98f065f890bdf02003b
y.mitryasov <yrkash@gmail.com>
2024-05-08 15:51:32 +0300

    92 Reverse Linked List

---
+++ src/ru/yrkash/task92/Solution.java
@@ -0,0 +1,48 @@
+package ru.yrkash.task92;
+
+
+import java.util.Stack;
+
+public class Solution {
+
+    public ListNode reverseBetween(ListNode head, int left, int right) {
+        if (head == null) {
+            return null;
+        }
+
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+        ListNode pre = dummy;
+
+        for (int i = 0; i < left - 1; i++) {
+            pre = pre.next;
+        }
+
+        ListNode start = pre.next;
+        ListNode then = start.next;
+
+        for (int i = 0; i < right - left; i++) {
+            start.next = then.next;
+            then.next = pre.next;
+            pre.next = then;
+            then = start.next;
+        }
+
+        return dummy.next;
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        ListNode node_1 = new ListNode(3);
+        ListNode node_2 = new ListNode(5);
+//        ListNode node_3 = new ListNode(3);
+//        ListNode node_4 = new ListNode(4);
+//        ListNode node_5 = new ListNode(5);
+        node_1.next = node_2;
+//        node_2.next = node_3;
+//        node_3.next = node_4;
+//        node_4.next = node_5;
+        ListNode result = solution.reverseBetween(node_1, 1, 2);
+        System.out.println(result.val);
+    }
+}
