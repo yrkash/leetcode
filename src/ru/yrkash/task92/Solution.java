a6ec6dff651d545548d9074205c8a2c8e304bd43
y.mitryasov <yrkash@gmail.com>
2024-05-13 15:44:04 +0300

    92 Reverse part of Linked List

--- src/ru/yrkash/task92/Solution.java
+++ src/ru/yrkash/task92/Solution.java
@@ -21,6 +21,7 @@ public class Solution {
         ListNode start = pre.next;
         ListNode then = start.next;
 
+        // Swap of 3 elements ( previous, start, then) and go right for 1 step
         for (int i = 0; i < right - left; i++) {
             start.next = then.next;
             then.next = pre.next;
@@ -33,16 +34,16 @@ public class Solution {
 
     public static void main(String[] args) {
         Solution solution = new Solution();
-        ListNode node_1 = new ListNode(3);
-        ListNode node_2 = new ListNode(5);
-//        ListNode node_3 = new ListNode(3);
-//        ListNode node_4 = new ListNode(4);
-//        ListNode node_5 = new ListNode(5);
+        ListNode node_1 = new ListNode(1);
+        ListNode node_2 = new ListNode(2);
+        ListNode node_3 = new ListNode(3);
+        ListNode node_4 = new ListNode(4);
+        ListNode node_5 = new ListNode(5);
         node_1.next = node_2;
-//        node_2.next = node_3;
-//        node_3.next = node_4;
-//        node_4.next = node_5;
-        ListNode result = solution.reverseBetween(node_1, 1, 2);
+        node_2.next = node_3;
+        node_3.next = node_4;
+        node_4.next = node_5;
+        ListNode result = solution.reverseBetween(node_1, 2, 4);
         System.out.println(result.val);
     }
 }
